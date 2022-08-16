dff7f4e915b574b3f79ef114df5c1cec0f022396
MrCompoopter <christian.putra036@proton.me>
2022-08-16 18:42:18 +0700

    remove to merge

--- ganjilgenap.lua
+++
@@ -1,9 +0,0 @@
--- non functional
-print ('ayooooo, mau cek ganjil genap ya ?? tulis dibawah!!')
-io.write ("input lo : ")
-input = (io.read())
-if (input % 2) == (0) then
-    print ("genap")
-    else
-    print ("ganjil")
-end
