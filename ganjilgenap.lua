827f55f2ab7c685accd9fef0940467c0d6a410ea
MrCompoopter <christian.putra036@proton.me>
2022-08-16 18:10:05 +0700

    solved it

--- ganjilgenap.lua
+++ ganjilgenap.lua
@@ -1,1 +1,9 @@
- 
+-- non functional
+print ('ayooooo, mau cek ganjil genap ya ?? tulis dibawah!!')
+io.write ("input lo : ")
+input = (io.read())
+if (input % 2) == (0) then
+    print ("genap")
+    else
+    print ("ganjil")
+end
